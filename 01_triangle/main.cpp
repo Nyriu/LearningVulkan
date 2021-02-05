352162a7dc70a69b2b12c662fdafb23dcc58d719
Nyriu <muninitristano@gmail.com>
2021-02-05 22:38:35 +0100

    Shader modules

--- 01_triangle/main.cpp
+++ 01_triangle/main.cpp
@@ -10,6 +10,7 @@
 #include <cstring>
 #include <optional>
 #include <set>
+#include <fstream>
 
 //#include <cstdint>
 //#include <algorithm>
@@ -72,6 +73,9 @@ struct SwapChainSupportDetails {
 
 
 
+
+
+
 class HelloTriangleApplication {
 public:
     void run() {
@@ -119,6 +123,7 @@ private:
       createLogicalDevice();
       createSwapChain();
       createImageViews();
+      createGraphicsPipeline();
     }
 
     bool isDeviceSuitable(VkPhysicalDevice device) {
@@ -539,6 +544,73 @@ private:
         throw std::runtime_error("failed to create instance!");
       }
     }
+
+
+    static std::vector<char> readFile(const std::string& filename) {
+      std::ifstream file(filename, std::ios::ate | std::ios::binary);
+      // ate = start reading from EOF
+      // trick to get the file len and inst a vector
+
+      if (!file.is_open()) {
+        throw std::runtime_error("failed to open file!");
+      }
+
+      size_t fileSize = (size_t) file.tellg();
+      std::vector<char> buffer(fileSize);
+
+      file.seekg(0);
+      file.read(buffer.data(), fileSize);
+
+      file.close();
+
+      return buffer;
+    }
+
+    void createGraphicsPipeline() {
+      auto vertShaderCode = readFile("shaders/vert.spv");
+      auto fragShaderCode = readFile("shaders/frag.spv");
+
+      VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+      VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+      VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+      vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+      vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+      vertShaderStageInfo.module = vertShaderModule;
+      vertShaderStageInfo.pName = "main"; // we can specify a different entrypoint
+      vertShaderStageInfo.pSpecializationInfo = nullptr; // specify values for shader constants
+
+      VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+      fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+      fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+      fragShaderStageInfo.module = fragShaderModule;
+      fragShaderStageInfo.pName = "main";
+
+      VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
+
+
+
+
+      vkDestroyShaderModule(device, fragShaderModule, nullptr);
+      vkDestroyShaderModule(device, vertShaderModule, nullptr);
+    }
+
+    VkShaderModule createShaderModule(const std::vector<char>& code) {
+
+      VkShaderModuleCreateInfo createInfo{};
+      createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+      createInfo.codeSize = code.size();
+      createInfo.pCode = reinterpret_cast<const uint32_t*>(code.data()); // tricky step // beware of alignment between char and uint32_t
+
+      VkShaderModule shaderModule;
+      if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create shader module!");
+      }
+
+      return shaderModule;
+    }
+
 
 
     void mainLoop() {
