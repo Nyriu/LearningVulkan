b1f3eaa713a18b773d1129484cb707c46c0f43de
Nyriu <muninitristano@gmail.com>
2021-03-05 09:38:43 +0100

    graphics pipeline

--- 01_triangle/main.cpp
+++ 01_triangle/main.cpp
@@ -108,6 +108,8 @@ private:
     VkRenderPass renderPass;
     VkPipelineLayout pipelineLayout; // circa uniforms for shaders
 
+    VkPipeline graphicsPipeline;
+
 
 
     void initWindow() {
@@ -732,6 +734,36 @@ private:
         throw std::runtime_error("failed to create pipeline layout!");
       }
 
+
+
+      VkGraphicsPipelineCreateInfo pipelineInfo{};
+      pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+      pipelineInfo.stageCount = 2;
+      pipelineInfo.pStages = shaderStages;
+
+      pipelineInfo.pVertexInputState = &vertexInputInfo;
+      pipelineInfo.pInputAssemblyState = &inputAssembly;
+      pipelineInfo.pViewportState = &viewportState;
+      pipelineInfo.pRasterizationState = &rasterizer;
+      pipelineInfo.pMultisampleState = &multisampling;
+      pipelineInfo.pDepthStencilState = nullptr; // Optional
+      pipelineInfo.pColorBlendState = &colorBlending;
+      pipelineInfo.pDynamicState = nullptr; // Optional
+
+      pipelineInfo.layout = pipelineLayout;
+
+      pipelineInfo.renderPass = renderPass;
+      pipelineInfo.subpass = 0;
+
+
+      pipelineInfo.basePipelineHandle = VK_NULL_HANDLE; // Optional
+      pipelineInfo.basePipelineIndex = -1; // Optional
+
+      if (vkCreateGraphicsPipelines(device, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &graphicsPipeline) != VK_SUCCESS) {
+        throw std::runtime_error("failed to create graphics pipeline!");
+      }
+
+
       vkDestroyShaderModule(device, fragShaderModule, nullptr);
       vkDestroyShaderModule(device, vertShaderModule, nullptr);
     }
@@ -761,6 +793,7 @@ private:
 
     void cleanup() {
 
+      vkDestroyPipeline(device, graphicsPipeline, nullptr);
       vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
 
       vkDestroyRenderPass(device, renderPass, nullptr);
